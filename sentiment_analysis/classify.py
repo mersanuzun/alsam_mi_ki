71224e7ff5d9444c83d7f2b810f04d235f4bd18d
mersanuzun <mersanuzun@gmail.com>
2016-03-29 17:08:31 +0300

    naive bayes classification algoritim

--- sentiment_analysis/classify.py
+++ sentiment_analysis/classify.py
@@ -1,13 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 from textblob.classifiers import NaiveBayesClassifier
 
-train_data = [('bu urun cok guzel', 'pos'),
-              ('cok memnunum', 'pos'),
-              ('Cok uygun fiyata cok guzel urun', 'pos'),
+train_data = [("bu ürün çok güzel".decode('utf8'), 'pos'),
+              ('çok memnunum'.decode('utf8'), 'pos'),
+              ('Çok uygun fiyata çok güzel ürün'.decode('utf8'), 'pos'),
               ('Tek kelimeyle harika', 'pos'),
-              ('begenmedim', 'neg'),
-              ('hic iyi bir urun degil', 'neg'),
-              ('almayin bence', 'neg')]
+              ('beğenmedim'.decode('utf-8'), 'neg'),
+              ('hiç iyi bir ürün değil'.decode('utf8'), 'neg'),
+              ('almayın bence'.decode('utf-8'), 'neg')]
 
 cl = NaiveBayesClassifier(train_data)
-print cl.classify('guzel') # pos
-print cl.classify('almayin') #neg
+print cl.classify('bu ürün çok güzel') # pos
+print cl.classify('almayın') #neg
+print cl.classify('çok memnunum') #pos
+print cl.classify('beğenmedim') #neg
+print cl.classify('Tek kelimeyle memnunum') #pos
