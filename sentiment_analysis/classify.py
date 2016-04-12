ac9b04bae362045f71b1ccf69afa1ff31c0064b6
mersanuzun <mersanuzun@gmail.com>
2016-04-12 20:29:09 +0300

    training naive bayes classifier

--- sentiment_analysis/classify.py
+++ sentiment_analysis/classify.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from textblob.classifiers import NaiveBayesClassifier
+import codecs
 
 train_data = [("bu ürün çok güzel".decode('utf8'), 'pos'),
               ('çok memnunum'.decode('utf8'), 'pos'),
@@ -10,9 +11,15 @@ train_data = [("bu ürün çok güzel".decode('utf8'), 'pos'),
               ('hiç iyi bir ürün değil'.decode('utf8'), 'neg'),
               ('almayın bence'.decode('utf-8'), 'neg')]
 
-cl = NaiveBayesClassifier(train_data)
-print cl.classify('bu ürün çok güzel') # pos
-print cl.classify('almayın') #neg
-print cl.classify('çok memnunum') #pos
-print cl.classify('beğenmedim') #neg
-print cl.classify('Tek kelimeyle memnunum') #pos
+reviews = []
+with open('../scraping_reviews/pos.txt') as file: 
+   for line in file:
+       reviews.append((line.decode('utf8'), "pos"))
+with open('../scraping_reviews/neg.txt') as file:
+   for line in file:
+       reviews.append((line.decode('utf8'), 'neg'))
+print reviews[len(reviews) - 2]
+
+cl = NaiveBayesClassifier(reviews)
+print cl.classify('ürünü gerçekten beğenmedim'.decode('utf8'))
+
